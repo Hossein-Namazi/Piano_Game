99148b35a2ecd4d284d0a1cc7c8dc7aa73b26a54
Hossein <nmzhsen@gmail.com>
2025-05-16 05:55:11 -0700

    UI_Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿
+namespace Piano_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b14 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b24 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b34 = new System.Windows.Forms.Button();
+            this.b41 = new System.Windows.Forms.Button();
+            this.b42 = new System.Windows.Forms.Button();
+            this.b43 = new System.Windows.Forms.Button();
+            this.b44 = new System.Windows.Forms.Button();
+            this.Line_Red = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.T_Lable = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b11.Enabled = false;
+            this.b11.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(0, 0);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(100, 150);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = false;
+            // 
+            // b12
+            // 
+            this.b12.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b12.Enabled = false;
+            this.b12.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(-1, 150);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(100, 150);
+            this.b12.TabIndex = 1;
+            this.b12.UseVisualStyleBackColor = false;
+            // 
+            // b13
+            // 
+            this.b13.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.b13.Enabled = false;
+            this.b13.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.Location = new System.Drawing.Point(-1, 300);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(100, 150);
+            this.b13.TabIndex = 2;
+            this.b13.UseVisualStyleBackColor = false;
+            // 
+            // b14
+            // 
+            this.b14.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b14.Enabled = false;
+            this.b14.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b14.Location = new System.Drawing.Point(-1, 450);
+            this.b14.Name = "b14";
+            this.b14.Size = new System.Drawing.Size(100, 150);
+            this.b14.TabIndex = 3;
+            this.b14.UseVisualStyleBackColor = false;
+            // 
+            // b21
+            // 
+            this.b21.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.b21.Enabled = false;
+            this.b21.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(100, 0);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(100, 150);
+            this.b21.TabIndex = 4;
+            this.b21.UseVisualStyleBackColor = false;
+            // 
+            // b22
+            // 
+            this.b22.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b22.Enabled = false;
+            this.b22.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(97, 148);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(100, 150);
+            this.b22.TabIndex = 5;
+            this.b22.UseVisualStyleBackColor = false;
+            // 
+            // b23
+            // 
+            this.b23.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b23.Enabled = false;
+            this.b23.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.Location = new System.Drawing.Point(96, 296);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(100, 150);
+            this.b23.TabIndex = 6;
+            this.b23.UseVisualStyleBackColor = false;
+            // 
+            // b24
+            // 
+            this.b24.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b24.Enabled = false;
+            this.b24.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b24.Location = new System.Drawing.Point(96, 444);
+            this.b24.Name = "b24";
+            this.b24.Size = new System.Drawing.Size(100, 150);
+            this.b24.TabIndex = 7;
+            this.b24.UseVisualStyleBackColor = false;
+            // 
+            // b31
+            // 
+            this.b31.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b31.Enabled = false;
+            this.b31.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.Location = new System.Drawing.Point(200, 0);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(100, 150);
+            this.b31.TabIndex = 8;
+            this.b31.UseVisualStyleBackColor = false;
+            // 
+            // b32
+            // 
+            this.b32.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b32.Enabled = false;
+            this.b32.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.Location = new System.Drawing.Point(194, 148);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(100, 150);
+            this.b32.TabIndex = 9;
+            this.b32.UseVisualStyleBackColor = false;
+            // 
+            // b33
+            // 
+            this.b33.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b33.Enabled = false;
+            this.b33.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.Location = new System.Drawing.Point(195, 296);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(100, 150);
+            this.b33.TabIndex = 10;
+            this.b33.UseVisualStyleBackColor = false;
+            // 
+            // b34
+            // 
+            this.b34.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.b34.Enabled = false;
+            this.b34.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b34.Location = new System.Drawing.Point(194, 444);
+            this.b34.Name = "b34";
+            this.b34.Size = new System.Drawing.Size(100, 150);
+            this.b34.TabIndex = 11;
+            this.b34.UseVisualStyleBackColor = false;
+            // 
+            // b41
+            // 
+            this.b41.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b41.Enabled = false;
+            this.b41.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b41.Location = new System.Drawing.Point(300, 0);
+            this.b41.Name = "b41";
+            this.b41.Size = new System.Drawing.Size(100, 150);
+            this.b41.TabIndex = 12;
+            this.b41.UseVisualStyleBackColor = false;
+            // 
+            // b42
+            // 
+            this.b42.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.b42.Enabled = false;
+            this.b42.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b42.Location = new System.Drawing.Point(292, 148);
+            this.b42.Name = "b42";
+            this.b42.Size = new System.Drawing.Size(100, 150);
+            this.b42.TabIndex = 13;
+            this.b42.UseVisualStyleBackColor = false;
+            // 
+            // b43
+            // 
+            this.b43.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b43.Enabled = false;
+            this.b43.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b43.Location = new System.Drawing.Point(292, 296);
+            this.b43.Name = "b43";
+            this.b43.Size = new System.Drawing.Size(100, 150);
+            this.b43.TabIndex = 14;
+            this.b43.UseVisualStyleBackColor = false;
+            // 
+            // b44
+            // 
+            this.b44.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.b44.Enabled = false;
+            this.b44.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b44.Location = new System.Drawing.Point(292, 444);
+            this.b44.Name = "b44";
+            this.b44.Size = new System.Drawing.Size(100, 150);
+            this.b44.TabIndex = 15;
+            this.b44.UseVisualStyleBackColor = false;
+            // 
+            // Line_Red
+            // 
+            this.Line_Red.BackColor = System.Drawing.Color.DarkRed;
+            this.Line_Red.Enabled = false;
+            this.Line_Red.Location = new System.Drawing.Point(-36, 520);
+            this.Line_Red.Name = "Line_Red";
+            this.Line_Red.Size = new System.Drawing.Size(489, 1);
+            this.Line_Red.TabIndex = 28;
+            this.Line_Red.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 507);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 36);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "F";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label2.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 501);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 36);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.label3.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(230, 507);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 36);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "H";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label4.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(326, 507);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 36);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "J";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.Font = new System.Drawing.Font("Ravie", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(100, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 294);
+            this.button1.TabIndex = 37;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // T_Lable
+            // 
+            this.T_Lable.AutoSize = true;
+            this.T_Lable.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.T_Lable.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.T_Lable.ForeColor = System.Drawing.Color.Maroon;
+            this.T_Lable.Location = new System.Drawing.Point(12, 9);
+            this.T_Lable.Name = "T_Lable";
+            this.T_Lable.Size = new System.Drawing.Size(37, 22);
+            this.T_Lable.TabIndex = 38;
+            this.T_Lable.Text = "30";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label5.Font = new System.Drawing.Font("Ravie", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(308, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 34);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "New \r\nRecorard";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(396, 597);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.T_Lable);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Line_Red);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b43);
+            this.Controls.Add(this.b42);
+            this.Controls.Add(this.b41);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b44);
+            this.Controls.Add(this.b34);
+            this.Controls.Add(this.b24);
+            this.Controls.Add(this.b14);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b14;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b24;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b34;
+        private System.Windows.Forms.Button b41;
+        private System.Windows.Forms.Button b42;
+        private System.Windows.Forms.Button b43;
+        private System.Windows.Forms.Button b44;
+        private System.Windows.Forms.Button Line_Red;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label T_Lable;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
